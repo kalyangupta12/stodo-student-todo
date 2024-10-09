f8bd7733f953969e4f6038520453e323b9b7c20e
KALYAN GUPTA <kalyangupta2002@gmail.com>
2024-10-09 17:52:15 +0530

    navbar major fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ function StickyNavbar() {
   );
 
   return (
-    <Navbar className="sticky top-0 z-10 w-full bg-white shadow-lg py-2 px-4 lg:px-8">
+    <Navbar className="sticky mx-auto w-full px-4 py-2 lg:px-8 lg:py-4 bg-white">
       <div className="container mx-auto flex items-center justify-between">
         <Typography
           as="a"
